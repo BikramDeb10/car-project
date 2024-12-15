09f4f6e516b4680a0cd7b3f3e812c045be7af79f
gupi debnath <iamgupidebnath@gmail.com>
2024-12-15 22:40:43 +0530

    rider and about update

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-const menuButton = document.getElementById("menuButton");
-const mobileMenu = document.getElementById("mobileMenu");
+const menuButton = document.getElementById('menuButton');
+const mobileMenu = document.getElementById('mobileMenu');
 
-menuButton.addEventListener("click", () => {
-  const isExpanded = menuButton.getAttribute("aria-expanded") === "true";
-  menuButton.setAttribute("aria-expanded", !isExpanded);
-  mobileMenu.classList.toggle("hidden");
+menuButton.addEventListener('click', () => {
+  const isExpanded = menuButton.getAttribute('aria-expanded') === 'true';
+  menuButton.setAttribute('aria-expanded', !isExpanded);
+  mobileMenu.classList.toggle('hidden');
 
   if (!isExpanded) {
-    document.addEventListener("click", handleOutsideClick);
+    document.addEventListener('click', handleOutsideClick);
   } else {
-    document.removeEventListener("click", handleOutsideClick);
+    document.removeEventListener('click', handleOutsideClick);
   }
 });
 
@@ -18,56 +18,56 @@ function handleOutsideClick(event) {
     !mobileMenu.contains(event.target) &&
     !menuButton.contains(event.target)
   ) {
-    mobileMenu.classList.add("hidden");
-    menuButton.setAttribute("aria-expanded", "false");
-    document.removeEventListener("click", handleOutsideClick);
+    mobileMenu.classList.add('hidden');
+    menuButton.setAttribute('aria-expanded', 'false');
+    document.removeEventListener('click', handleOutsideClick);
   }
 }
 
-document.addEventListener("keydown", (event) => {
-  if (event.key === "Escape" && !mobileMenu.classList.contains("hidden")) {
-    mobileMenu.classList.add("hidden");
-    menuButton.setAttribute("aria-expanded", "false");
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape' && !mobileMenu.classList.contains('hidden')) {
+    mobileMenu.classList.add('hidden');
+    menuButton.setAttribute('aria-expanded', 'false');
   }
 });
 
 //scroll
-const carousel = document.querySelector("#carousel");
-const prevButton = document.querySelector("#prevButton");
-const nextButton = document.querySelector("#nextButton");
+const carousel = document.querySelector('#carousel');
+const prevButton = document.querySelector('#prevButton');
+const nextButton = document.querySelector('#nextButton');
 
 if (carousel && prevButton && nextButton) {
-  prevButton.addEventListener("click", () => {
+  prevButton.addEventListener('click', () => {
     carousel.scrollBy({
       left: -390 * 1,
-      behavior: "smooth",
+      behavior: 'smooth',
     });
   });
 
-  nextButton.addEventListener("click", () => {
+  nextButton.addEventListener('click', () => {
     carousel.scrollBy({
       left: 390 * 1,
-      behavior: "smooth",
+      behavior: 'smooth',
     });
   });
 }
 
-const carousel2 = document.querySelector("#carousel2");
-const prevButton2 = document.querySelector("#prevButton2");
-const nextButton2 = document.querySelector("#nextButton2");
+const carousel2 = document.querySelector('#carousel2');
+const prevButton2 = document.querySelector('#prevButton2');
+const nextButton2 = document.querySelector('#nextButton2');
 
 if (carousel2 && prevButton2 && nextButton2) {
-  prevButton2.addEventListener("click", () => {
+  prevButton2.addEventListener('click', () => {
     carousel2.scrollBy({
-      left: -390 * 1,
-      behavior: "smooth",
+      left: -400 * 1,
+      behavior: 'smooth',
     });
   });
 
-  nextButton2.addEventListener("click", () => {
+  nextButton2.addEventListener('click', () => {
     carousel2.scrollBy({
-      left: 390 * 1,
-      behavior: "smooth",
+      left: 400 * 1,
+      behavior: 'smooth',
     });
   });
 }
